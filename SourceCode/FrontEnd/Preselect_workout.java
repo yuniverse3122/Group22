73fc39f2e1d8a955174edcc03d576d9fe1ab5fd6
yuniverse3122 <66040230+yuniverse3122@users.noreply.github.com>
2021-03-07 23:56:29 -0500

    Boxing1.1

---
+++ SourceCode/FrontEnd/Preselect_workout.java
@@ -0,0 +1,14 @@
+package com.example.boxing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Preselect_workout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preselect_workout);
+    }
+}
---
+++ SourceCode/FrontEnd/User.java
@@ -0,0 +1,19 @@
+package com.example.boxing;
+
+public class User {
+    public String email, password;
+
+    public User(){
+
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+
+
+
+
+}
