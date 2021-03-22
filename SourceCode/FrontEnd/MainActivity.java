4012bda7e2cd67e9829727630a9fcc87ff317ee7
yuniverse3122 <66040230+yuniverse3122@users.noreply.github.com>
2021-03-21 23:14:14 -0400

    Update MainActivity.java

--- SourceCode/FrontEnd/MainActivity.java
+++ SourceCode/FrontEnd/MainActivity.java
@@ -7,12 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import static android.content.ContentValues.TAG;
+
+
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button signOut;
-    private Button history, preSelectedWorkout,freeMode;
+    private Button history, preSelectedWorkout,freeMode, connect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         signOut.setOnClickListener(this);
         preSelectedWorkout.setOnClickListener(this);
         freeMode.setOnClickListener(this);
+
 
     }
 
@@ -46,6 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.History:
                 startActivity(new Intent(MainActivity.this, History.class));
                 break;
+
         }
     }
 }
