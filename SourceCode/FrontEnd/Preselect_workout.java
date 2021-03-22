f64647d41031c65c75d8c7f32682cb910561c604
yuniverse3122 <66040230+yuniverse3122@users.noreply.github.com>
2021-03-21 23:14:40 -0400

    Update Preselect_workout.java

--- SourceCode/FrontEnd/Preselect_workout.java
+++ SourceCode/FrontEnd/Preselect_workout.java
@@ -2,12 +2,10 @@ package com.example.boxing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-
-import com.google.firebase.auth.FirebaseAuth;
 
 public class Preselect_workout extends AppCompatActivity {
 
@@ -17,12 +15,4 @@ public class Preselect_workout extends AppCompatActivity {
         setContentView(R.layout.activity_preselect_workout);
     }
 
-    @Override
-    public void onclick(View v){
-      switch (v.getID()){
-        case R.id.StartWorkout:
-        Workout data = new Workout(System.currentTimeMillis());
-        data.workoutType = "Preselected";
-      }
-    }
 }
