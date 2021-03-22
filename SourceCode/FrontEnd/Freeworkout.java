a911084854be69430df150b7bf16bf6ba5554046
dag13 <65682018+dag13@users.noreply.github.com>
2021-03-21 21:08:53 -0400

    Update Freeworkout.java

--- SourceCode/FrontEnd/Freeworkout.java
+++ SourceCode/FrontEnd/Freeworkout.java
@@ -3,6 +3,10 @@ package com.example.boxing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Freeworkout extends AppCompatActivity {
 
@@ -11,4 +15,19 @@ public class Freeworkout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_freeworkout);
     }
+    
+     @Override
+    public void onclick(View v){
+      WorkoutData data = new Workout(System.currentTimeMillis());
+      switch (v.getID()){
+        case R.id.Beginner:
+        data.workoutType = "Beginner Free Workout";
+        break;
+        case R.id.Intermediate:
+        data.workoutType = "Intermediate Free Workout";
+        break;
+        case R.id.Advanced:
+        data.workoutType = "Advanced Free Workout";
+        break;
+      }
 }
