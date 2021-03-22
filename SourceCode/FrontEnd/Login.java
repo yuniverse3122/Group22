35739b97ad7b878c7db1117ecda8dd21f272ea3a
yuniverse3122 <66040230+yuniverse3122@users.noreply.github.com>
2021-03-21 23:12:33 -0400

    Update Login.java

--- SourceCode/FrontEnd/Login.java
+++ SourceCode/FrontEnd/Login.java
@@ -2,12 +2,30 @@ package com.example.boxing;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,20 +39,52 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
     private EditText  editTextEmail, editTextPassword ;
-    private Button sign_in, register;
+    private Button sign_in, register, connect;
     private ProgressBar bar;
     private FirebaseAuth mAuth;
+
+    private String deviceName = null;
+    private String deviceAddress;
+    public static Handler handler;
+    public static BluetoothSocket mmSocket;
+    /*
+    public static ConnectedThread connectedThread;
+    public static CreateConnectThread createConnectThread;
+     */
+
+    private final static int CONNECTING_STATUS = 1;
+    private final static int MESSAGE_READ = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        /*
+        final Button buttonConnect = findViewById(R.id.buttonConnect);
+
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+
+        deviceName = getIntent().getStringExtra("deviceName");
+        if(deviceName != null) {
+            deviceAdress = getIntent().getStringExtra("deviceAddress");
+            toolbar.setSubtitle("Connecting to " + deviceName + "...");
+            buttonConnect.setEnabled(false);
+
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            createConnectThread = new CreateConnectThread(bluetoothAdapter, deviceAddress);
+            createConnectThread.start();
+            */
         sign_in = findViewById(R.id.login);
         sign_in.setOnClickListener(this);
 
         register = findViewById(R.id.register);
         register.setOnClickListener(this);
+
+        connect = (Button) findViewById(R.id.button);
+        connect.setOnClickListener(this);
+
+
 
         editTextEmail = findViewById(R.id.username);
         editTextPassword = findViewById(R.id.password);
@@ -43,6 +93,10 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         mAuth = FirebaseAuth.getInstance();
 
     }
+
+
+
+
 
     @Override
     // two buttons on login page
@@ -53,6 +107,9 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 break;
             case R.id.register:
                 startActivity(new Intent(Login.this, Register.class));
+                break;
+            case R.id.button:
+                startActivity(new Intent(Login.this, SelectDeviceActivity.class));
                 break;
         }
     }
