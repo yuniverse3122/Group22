2e75bc5da34f1284bf7fd0ce64dbb82cb05f00b8
yuniverse3122 <66040230+yuniverse3122@users.noreply.github.com>
2021-03-21 23:10:21 -0400

    Update Freeworkout.java

--- SourceCode/FrontEnd/Freeworkout.java
+++ SourceCode/FrontEnd/Freeworkout.java
@@ -2,12 +2,11 @@ package com.example.boxing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.auth.FirebaseAuth;
 
 public class Freeworkout extends AppCompatActivity {
 
@@ -16,21 +15,4 @@ public class Freeworkout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_freeworkout);
     }
-
-    @Override
-    public void onclick(View v){
-      Workout data = new Workout(System.currentTimeMillis());
-      data.workoutType = "Free Workout";
-      switch (v.getID()){
-        case R.id.Beginner:
-        data.difficulty = 1;
-        break;
-        case R.id.Intermediate:
-        data.difficulty = 2;
-        break;
-        case R.id.Advanced:
-        data.difficulty = 3;
-        break;
-      }
 }
-
