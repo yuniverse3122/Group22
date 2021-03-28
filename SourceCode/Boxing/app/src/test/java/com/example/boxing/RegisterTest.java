d2b5a2a7ee1e3145fc6f014e528defccfdfc41a8
yuniverse3122 <66040230+yuniverse3122@users.noreply.github.com>
2021-03-28 17:17:06 -0400

    Organized Files

---
+++ SourceCode/Boxing/app/src/test/java/com/example/boxing/RegisterTest.java
@@ -0,0 +1,16 @@
+package com.example.boxing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RegisterTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void checkRegister() {
+    }
+}
