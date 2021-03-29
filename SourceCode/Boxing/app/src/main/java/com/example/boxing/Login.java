0e15a94a7ded35640413b0e43da39c28aa9f7c99
yuniverse3122 <66040230+yuniverse3122@users.noreply.github.com>
2021-03-28 22:52:15 -0400

    Update Login.java

--- SourceCode/Boxing/app/src/main/java/com/example/boxing/Login.java
+++ SourceCode/Boxing/app/src/main/java/com/example/boxing/Login.java
@@ -74,6 +74,224 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
             BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
             createConnectThread = new CreateConnectThread(bluetoothAdapter, deviceAddress);
             createConnectThread.start();
+
+          }
+          handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg){
+                switch (msg.what){
+                    case CONNECTING_STATUS:
+                        switch(msg.arg1){
+                            case 1:
+                                toolbar.setSubtitle("Connected to " + deviceName);
+                                progressBar.setVisibility(View.GONE);
+                                buttonConnect.setEnabled(true);
+                                buttonToggle.setEnabled(true);
+                                break;
+                            case -1:
+                                toolbar.setSubtitle("Device fails to connect");
+                                progressBar.setVisibility(View.GONE);
+                                buttonConnect.setEnabled(true);
+                                break;
+                        }
+                        break;
+
+                    case MESSAGE_READ:
+                        String arduinoMsg = msg.obj.toString(); // Read message from Arduino
+                        switch (arduinoMsg.toLowerCase()){
+                            case "led is turned on":
+                                imageView.setBackgroundColor(getResources().getColor(R.color.colorOn));
+                                textViewInfo.setText("Arduino Message : " + arduinoMsg);
+                                break;
+                            case "led is turned off":
+                                imageView.setBackgroundColor(getResources().getColor(R.color.colorOff));
+                                textViewInfo.setText("Arduino Message : " + arduinoMsg);
+                                break;
+                        }
+                        break;
+                }
+            }
+        };
+
+        // Select Bluetooth Device
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Move to adapter list
+                Intent intent = new Intent(MainActivity.this, SelectDeviceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Button to ON/OFF LED on Arduino Board
+        buttonToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String cmdText = null;
+                String btnState = buttonToggle.getText().toString().toLowerCase();
+                switch (btnState){
+                    case "turn on":
+                        buttonToggle.setText("Turn Off");
+                        // Command to turn on LED on Arduino. Must match with the command in Arduino code
+                        cmdText = "<turn on>";
+                        break;
+                    case "turn off":
+                        buttonToggle.setText("Turn On");
+                        // Command to turn off LED on Arduino. Must match with the command in Arduino code
+                        cmdText = "<turn off>";
+                        break;
+                }
+                // Send command to Arduino board
+                connectedThread.write(cmdText);
+            }
+        });
+    }
+
+
+        public static class CreateConnectThread extends Thread {
+
+            public CreateConnectThread(BluetoothAdapter bluetoothAdapter, String address) {
+
+                BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice(address);
+                BluetoothSocket tmp = null;
+                UUID uuid = bluetoothDevice.getUuids()[0].getUuid();
+
+                try {
+                /*
+                Get a BluetoothSocket to connect with the given BluetoothDevice.
+                Due to Android device varieties,the method below may not work fo different devices.
+                You should try using other methods i.e. :
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+
+                    tmp = bluetoothDevice.createInsecureRfcommSocketToServiceRecord(uuid);
+
+                } catch (IOException e) {
+                    Log.e(TAG, "Socket's create() method failed", e);
+                }
+                mmSocket = tmp;
+            }
+
+            public void run() {
+                // Cancel discovery because it otherwise slows down the connection.
+                BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                bluetoothAdapter.cancelDiscovery();
+                try {
+                    // Connect to the remote device through the socket. This call blocks
+                    // until it succeeds or throws an exception.
+                    mmSocket.connect();
+                    Log.e("Status", "Device connected");
+                    handler.obtainMessage(CONNECTING_STATUS, 1, -1).sendToTarget();
+                } catch (IOException connectException) {
+                    // Unable to connect; close the socket and return.
+                    try {
+                        mmSocket.close();
+                        Log.e("Status", "Cannot connect to device");
+                        handler.obtainMessage(CONNECTING_STATUS, -1, -1).sendToTarget();
+                    } catch (IOException closeException) {
+                        Log.e(TAG, "Could not close the client socket", closeException);
+                    }
+                    return;
+                }
+
+                // The connection attempt succeeded. Perform work associated with
+                // the connection in a separate thread.
+                connectedThread = new ConnectedThread(mmSocket);
+                connectedThread.run();
+            }
+
+            // Closes the client socket and causes the thread to finish.
+            public void cancel() {
+                try {
+                    mmSocket.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "Could not close the client socket", e);
+                }
+            }
+        }
+
+        /* =============================== Thread for Data Transfer ===========================================
+        public static class ConnectedThread extends Thread {
+            private final BluetoothSocket mmSocket;
+            private final InputStream mmInStream;
+            private final OutputStream mmOutStream;
+
+            public ConnectedThread(BluetoothSocket socket) {
+                mmSocket = socket;
+                InputStream tmpIn = null;
+                OutputStream tmpOut = null;
+
+                // Get the input and output streams, using temp objects because
+                // member streams are final
+                try {
+                    tmpIn = socket.getInputStream();
+                    tmpOut = socket.getOutputStream();
+                } catch (IOException e) { }
+
+                mmInStream = tmpIn;
+                mmOutStream = tmpOut;
+            }
+
+            public void run() {
+                byte[] buffer = new byte[1024];  // buffer store for the stream
+                int bytes = 0; // bytes returned from read()
+                // Keep listening to the InputStream until an exception occurs
+                while (true) {
+                    try {
+                    /*
+                    Read from the InputStream from Arduino until termination character is reached.
+                    Then send the whole String message to GUI Handler.
+
+                        buffer[bytes] = (byte) mmInStream.read();
+                        String readMessage;
+                        if (buffer[bytes] == '\n'){
+                            readMessage = new String(buffer,0,bytes);
+                            Log.e("Arduino Message",readMessage);
+                            handler.obtainMessage(MESSAGE_READ,readMessage).sendToTarget();
+                            bytes = 0;
+                        } else {
+                            bytes++;
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+                }
+            }
+
+            /* Call this from the main activity to send data to the remote device
+            public void write(String input) {
+                byte[] bytes = input.getBytes(); //converts entered String into bytes
+                try {
+                    mmOutStream.write(bytes);
+                } catch (IOException e) {
+                    Log.e("Send Error","Unable to send message",e);
+                }
+            }
+
+            /* Call this from the main activity to shutdown the connection
+            public void cancel() {
+                try {
+                    mmSocket.close();
+                } catch (IOException e) { }
+            }
+        }
+
+        /* ============================ Terminate Connection at BackPress ======================
+        @Override
+        public void onBackPressed() {
+            // Terminate Bluetooth Connection and close app
+            if (createConnectThread != null){
+                createConnectThread.cancel();
+            }
+            Intent a = new Intent(Intent.ACTION_MAIN);
+            a.addCategory(Intent.CATEGORY_HOME);
+            a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(a);
+        }
+    }
+
+
+
             */
         sign_in = findViewById(R.id.login);
         sign_in.setOnClickListener(this);
